2530db46e917ccd5d90e09f0ac79847ccb7d4bc2
Nguyễn Ngọc Quỳnh Giao <giaonguyennq142001@gmail.com>
2021-12-12 10:12:18 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,7 @@ const Button = styled.button`
   width: 40%;
   border: none;
   padding: 15px 20px;
-  background-color: darkorchid;
+  background-color: crimson;
   color: white;
   cursor: pointer;
   margin-bottom: 10px;
